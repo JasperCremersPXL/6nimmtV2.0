0fb4153ed20fc561ac7757e4c14379cc7a8579c2
jaspe <11800955@student.pxl.be>
2022-02-07 11:28:50 +0100

    main menu & player

---
+++ 6nimmt/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name) {
+            Name = name;
+        }
+    }
