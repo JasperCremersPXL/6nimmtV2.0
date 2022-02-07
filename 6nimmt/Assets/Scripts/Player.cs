24c4e3bfc4baf56f6cf552f9b818ea3b0871f309
NikkiBrulsPXL <11900461@student.pxl.be>
2022-02-07 12:58:53 +0100

    updated player class

--- 6nimmt/Assets/Scripts/Player.cs
+++ 6nimmt/Assets/Scripts/Player.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-public class Player
-{
-    public string Name { get; set; }
-
-    public Player(string name)
+    public class Player
     {
-        Name = name;
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public List<Card> CardsInHand { get; set; }
+        private List<Card> _cardsTaken;
+        private List<Vector3> _playerCardPositions;
+
+        public Player(string name) {
+            Name = name;
+            Score = 0;
+            CardsInHand = new List<Card>();
+            _cardsTaken = new List<Card>();
+            _playerCardPositions = new List<Vector3>();
+        }
+
+        public void LoadCards() 
+        {
+            
+        }
     }
-}
+
